c9929bfa6d50274fa89f7defadd4c049e387c72e
Дмитрий К <>
2022-04-11 00:29:30 +0000

    chess

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-n = int(input())
-r = 0
-if n <= 15527:
-  r = 0
-elif 15528 <= n <= 42707:
-  r = 15
-elif 42708 <= n <= 132406:
-  r = 25
-elif n >= 132407:
-  r = 28
-print(f'The tax for {n} is {r}%. That is {round(n * r / 100)} dollars!')
+n, k = int(input()), int(input())
+print(n ** 2 - k ** 2)
