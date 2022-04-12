1dc9b4c359bc759bb7a3daeb9910996c13dd4be1
Дмитрий К <103391902+dmitrykarpachev@users.noreply.github.com>
2022-04-12 01:31:30 +0000

    re-write

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from random import choices
+even = [0, 2, 4, 6, 8]
+dod = [1, 3, 5, 7, 9]
 
-alphabet = 'qwertyuiopasdfghjklzxcvbnm'
-vowels = 'aeiuojwyv'
-lst = choices(alphabet, k=20)
-print(list(filter(lambda x: x in vowels, lst)))
+my_sum = list(map(lambda elem: sum(elem), zip(even, dod)))
+remainders = list(map(lambda a: a % 3, my_sum))
+nonzero_remainders = list(filter(lambda a: a != 0, remainders))
+
+print(remainders, nonzero_remainders)
