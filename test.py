cc14eef35eb2926b81e31b0db395ff72097a928a
Дмитрий К <103391902+dmitrykarpachev@users.noreply.github.com>
2022-04-12 02:37:22 +0000

    vector sum

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-string1 = input()
-string2 = input()
-print(''.join([s1 + s2 for s1, s2 in zip(string1, string2)]))
+vec1 = tuple(map(int, input().split(' ')))
+vec2 = tuple(map(int, input().split(' ')))
+
+print(f'v1 = {vec1} & v2 = {vec2}')
+for v1, v2 in zip(vec1, vec2):
+    print(v1 + v2)
