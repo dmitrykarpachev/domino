726bfc66a4cf480b6aafbd1200e8d0887cfbb0b4
Дмитрий К <103391902+dmitrykarpachev@users.noreply.github.com>
2022-04-12 02:00:44 +0000

    NY temperature

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-from random import randint
+def celsius_to_fahrenheit(lst):
+    return list(map(lambda C: round((C * (9 / 5)), 1) + 32, lst))
 
-names = ['Денис', 'Дима', 'Ваня', 'Дима', 'Дима', 'Лёха', 'Вова', 'Вадим', 'Богдан', 'Юра', 'Антон', 'Артем', 'Костя']
-maths = [randint(25, 50) for _ in range(13)]
-physics = [randint(25, 50) for _ in range(13)]
-english = [randint(25, 50) for _ in range(13)]
 
-print([name for name, m, p, e in zip(names, maths, physics, english) if m + p + e >= 100])
+daily_temp_c = [20.5, 19, 15, 25, 27, 30, 31, 29, 26, 21,
+                19, 25, 27.5, 28, 26, 29.5, 31, 27.5, 26, 29,
+                18, 17.5, 17, 16.5, 19, 20, 25, 26.5, 27, 28,
+                20.5, 19, 25, 27.5, 28, 26, 15, 25, 27, 28]
+
+temp_above_80 = list(filter(lambda val: val > 80, celsius_to_fahrenheit(daily_temp_c)))
+print(temp_above_80)
+print(len(temp_above_80))
