27578ca5e9e1f8f8f607034b1bce77691086b7c3
Дмитрий К <103391902+dmitrykarpachev@users.noreply.github.com>
2022-04-12 02:37:58 +0000

    next

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-vec1 = tuple(map(int, input().split(' ')))
-vec2 = tuple(map(int, input().split(' ')))
-
-print(f'v1 = {vec1} & v2 = {vec2}')
-for v1, v2 in zip(vec1, vec2):
-    print(v1 + v2)
+# index error
