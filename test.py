2359282a943c86692d56f0a7ad4ea40c5c4df01f
Дмитрий К <103391902+dmitrykarpachev@users.noreply.github.com>
2022-04-14 02:06:04 +0000

    implement logic

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-# a
+try:
+  name, lastname = input().split(' ')
+  print(f'Welcome to our party, {name} {lastname}')
+except BaseException:
+  print('You need to enter exactly 2 words. Try again!')
